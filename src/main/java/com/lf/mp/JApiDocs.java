3615bcafe804b97e0d47e42a282ad72468402220
a982338665 <982338665@qq.com>
2020-07-13 21:28:58 +0800

    4.JApi文档初步生成html版

--- src/main/java/com/lf/mp/JApiDocs.java
+++ src/main/java/com/lf/mp/JApiDocs.java
@@ -11,13 +11,17 @@ import io.github.yedaxia.apidocs.DocsConfig;
  */
 public class JApiDocs {
 
+    /**
+     * 访问：http://localhost:8080/V1.0/index.html 获取接口文档
+     * @param args
+     */
     public static void main(String[] args) {
         DocsConfig config = new DocsConfig();
         config.setProjectPath("./"); // 项目根目录
 //        config.setProjectPath("D:/git-20200518/lf-mk-mybatisPlus"); // 项目根目录
         config.setProjectName("lf-mk-mybatisPlus"); // 项目名称
-        config.setApiVersion("V1.2");       // 声明该API的版本
-        config.setDocsPath(".\\src\\main\\resources\\docs"); // 生成API 文档所在目录
+        config.setApiVersion("V1.0");       // 声明该API的版本
+        config.setDocsPath(".\\src\\main\\resources\\META-INF\\resources"); // 生成API 文档所在目录
 //        config.setDocsPath("D:\\git-20200518\\lf-mk-mybatisPlus\\src\\main\\resources\\docs"); // 生成API 文档所在目录
         config.setAutoGenerate(Boolean.TRUE);  // 配置自动生成
         Docs.buildHtmlDocs(config); // 执行生成文档
