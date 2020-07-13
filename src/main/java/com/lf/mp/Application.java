1eef6fbf9c61a7b3b96a7c11ed40ed350ce7a6bc
a982338665 <982338665@qq.com>
2020-07-13 20:31:22 +0800

    打开调试，取出多余配置

--- src/main/java/com/lf/mp/Application.java
+++ src/main/java/com/lf/mp/Application.java
@@ -3,6 +3,7 @@ package com.lf.mp;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @MapperScan("com.lf.mp.dao")
 @SpringBootApplication
