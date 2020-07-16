f650cc2c762a661241d2a5368dce672f030d4f97
a982338665 <982338665@qq.com>
2020-07-16 14:14:42 +0800

    7.swagger集成--请求头添加token

--- src/main/java/com/lf/mp/conf/Swagger2Config.java
+++ src/main/java/com/lf/mp/conf/Swagger2Config.java
@@ -3,13 +3,19 @@ package com.lf.mp.conf;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2 // 启用swagger2
@@ -17,12 +23,19 @@ public class Swagger2Config {
 
     @Bean
     public Docket createRestApi() {
+        //=====添加head参数start============================
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<Parameter>();
+        tokenPar.name("Authorization").description("AccessToken令牌").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+        pars.add(tokenPar.build());
+        // =========添加head参数end==================
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.lf.mp.web")) //扫描路径
                 .paths(PathSelectors.any()) //定义哪些路径的接口需要生成文档
-                .build();
+                .build()
+                .globalOperationParameters(pars);
     }
 
     private ApiInfo apiInfo() {
