9c8d93f02047cad41673ea9d972df0bc29e7ea56
luofeng <982338665@qq.com>
2021-01-28 15:31:12 +0800

    添加全局token

--- src/main/java/com/lf/mp/conf/Swagger2Config.java
+++ src/main/java/com/lf/mp/conf/Swagger2Config.java
@@ -8,6 +8,7 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
@@ -35,8 +36,17 @@ public class Swagger2Config {
                 .apis(RequestHandlerSelectors.basePackage("com.lf.mp.web")) //扫描路径
                 .paths(PathSelectors.any()) //定义哪些路径的接口需要生成文档
                 .build()
-                .globalOperationParameters(pars);
+                .globalOperationParameters(pars) //配置每一个接口的 token
+                .securitySchemes(security());         //配置公共的 token
+
     }
+
+    private List<ApiKey> security() {
+        List<ApiKey> apiKeyList = new ArrayList<ApiKey>();
+        apiKeyList.add(new ApiKey("token", "token", "header"));
+        return apiKeyList;
+    }
+
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
